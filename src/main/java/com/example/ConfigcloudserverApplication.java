5666fdc39ba0c4bc71ab0afb4bdec40e51584e0d
Lakshmikanth Rao <lkanthrao@gmail.com>
2016-12-01 23:41:30 +0530

    fixed an issue that was not letting MS to be discovered by consul

--- src/main/java/com/example/ConfigcloudserverApplication.java
+++ src/main/java/com/example/ConfigcloudserverApplication.java
@@ -2,11 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConfigcloudserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigcloudserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigcloudserverApplication.class, args);
+    }
 }
